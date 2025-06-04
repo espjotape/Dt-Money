d00922a5dc05db34a8b90c3b49493df971d82a5a
João Pedro <joaope.espindola2005@gmail.com>
2025-06-04 11:29:15 -0300

    Creating an affordable modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logo from "../../assets/logo.svg"
+import * as Dialog from '@radix-ui/react-dialog'
 
 export function Header() {
  return(
@@ -7,8 +8,20 @@ export function Header() {
    <HeaderContent>
     <img src={logo} alt="" />
 
-    <NewTransactionButton>Nova Transação</NewTransactionButton>
+    <Dialog.Root>
+     <Dialog.Trigger asChild>
+      <NewTransactionButton>Nova Transação</NewTransactionButton>
+     </Dialog.Trigger>
 
+     <Dialog.Portal>
+      <Dialog.Overlay />
+
+      <Dialog.Content>
+       <Dialog.Title>Nova Transação</Dialog.Title>
+       <Dialog.Close/>
+      </Dialog.Content>
+     </Dialog.Portal>
+    </Dialog.Root>
    </HeaderContent>
   </HeaderContainer>
  )

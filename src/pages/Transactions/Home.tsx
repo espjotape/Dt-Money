69cc8a1e4d1aa94d5ef474bc87af70ba70e41423
João Pedro <joaope.espindola2005@gmail.com>
2025-06-09 14:06:38 -0300

    API Connection - Performing HTTP Request

--- src/pages/Transactions/Home.tsx
+++ src/pages/Transactions/Home.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface Transaction {
+ id: number;
+ description: string;
+ type: 'income' | 'outcome';
+ price: number;
+ category: string;
+ createdAt: string;
+}
+
 export function Transactions() {
+ const [transactions, setTransactions] = useState<Transaction[]>([])
+ 
+ async function loadTransactions() {
+  const response = await fetch('http://localhost:3333/transactions')
+  const data = await response.json()
+
+  setTransactions(data)
+ }
+
+ useEffect(() => {
+  loadTransactions()
+ }, 
+ [])
+
  return (
   <div>
    <Header />
@@ -13,27 +37,20 @@ export function Transactions() {
     <SearchForm />
     <TransactionsTable>
     <tbody>
-     <tr>
-      <td>Desenvolvimento do site</td>
-      <td>
-       <PriceHighlight variant="incone">
-        R$ 12.000,00
-       </PriceHighlight>
-      </td>
-      <td>Venda</td>
-      <td>13/04/2022</td>
-     </tr>
-
-      <tr>
-      <td>Hambúrguer</td>
-      <td>
-       <PriceHighlight variant="outcone">
-        - R$ 59,00
-       </PriceHighlight>
-      </td>
-      <td>Alimentação</td>
-      <td>10/04/2022</td>
-     </tr>
+     {transactions.map(transaction => {
+      return (
+       <tr key={transaction.id}>
+       <td>{transaction.description}</td>
+       <td>
+        <PriceHighlight variant="incone">
+         {transaction.price}
+        </PriceHighlight>
+       </td>
+       <td>{transaction.type}</td>
+       <td>{transaction.createdAt}</td>
+      </tr>
+      )
+     })}
     </tbody>
    </TransactionsTable>
    </TransactionsContainer>
